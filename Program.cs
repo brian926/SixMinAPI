cd988d271af390eecf44385eeb4123886593c9ed
Brian Antunes <brian.antunes@yahoo.com>
2023-03-29 20:23:12 -0400

    Added dto and data.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var sqlConnBuilder = new SqlConnectionStringBuilder();
+
+sqlConnBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConnBuilder.UserID = builder.Configuration["UserId"];
+sqlConnBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnBuilder.ConnectionString));
 
 var app = builder.Build();
 
